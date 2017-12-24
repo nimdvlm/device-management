e5eb5620dd267d8934b2d461b2060c9fc559747b
cc19931002 <1181018082@qq.com>
2017-12-24 10:50:09 +0800

    登录跳转

--- src/main/java/cn/edu/bupt/utils/HttpUtil.java
+++ src/main/java/cn/edu/bupt/utils/HttpUtil.java
@@ -3,6 +3,7 @@ package cn.edu.bupt.utils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import okhttp3.*;
 
 import javax.servlet.http.HttpSession;
@@ -70,12 +71,12 @@ public class HttpUtil {
     }
 
     public static boolean getAccessToken(HttpSession session){
-        Object username = session.getAttribute("username");
-        Object password = session.getAttribute("username");
+        JsonPrimitive username = (JsonPrimitive)session.getAttribute("username");
+        JsonPrimitive password = (JsonPrimitive)session.getAttribute("password");
         if(username==null||password==null) return false;
         JsonObject json = new JsonObject();
-        json.addProperty("username",(String)username);
-        json.addProperty("password",(String)password);
+        json.addProperty("username",username.getAsString());
+        json.addProperty("password",password.getAsString());
         RequestBody body = RequestBody.create(JSON, json.toString());
         Request.Builder buider = new Request.Builder()
                 .url(tockenurl)
@@ -87,8 +88,8 @@ public class HttpUtil {
             if(response.isSuccessful()){
                 String res = response.body().string();
                 JsonObject obj = new JsonParser().parse(res).getAsJsonObject();
-                session.setAttribute("tocken",obj.get("tocken").getAsString());
-                session.setAttribute("refreshToken","obj.get(\"tocken\").getAsString()");
+                session.setAttribute("token",obj.get("token").getAsString());
+                session.setAttribute("refreshToken",obj.get("refreshToken").getAsString());
                 return true;
             }else{
                 throw new Exception("the first fail!") ;
