5567388118522d178927e7aab1de9fb010a145f7
cc <1181018082@qq.com>
2017-12-24 10:08:56 +0800

    login page

--- src/main/java/cn/edu/bupt/filter/AuthFilter.java
+++ src/main/java/cn/edu/bupt/filter/AuthFilter.java
@@ -18,36 +18,37 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest requ = (HttpServletRequest)servletRequest;
-        HttpServletResponse resp = (HttpServletResponse)servletResponse;
-        String path = requ.getRequestURI();
-        String method = requ.getMethod().toLowerCase();
-        if(path.endsWith(".html")||path.endsWith(".css")||path.endsWith(".js")||path.endsWith(".png")||path.endsWith(".jpg")||path.endsWith(".db ")){
-            filterChain.doFilter(servletRequest,servletResponse);
-            return ;
-        }
-        if(method.equals("post")){
-            Object username = requ.getSession().getAttribute("username");
-            Object password = requ.getSession().getAttribute("password");
-            if(username!=null&&password!=null){
-                filterChain.doFilter(servletRequest,servletResponse);
-            }else{
-                resp.sendRedirect("/login");
-            }
-        }else{
-            if(path.equals("/login")){
-                filterChain.doFilter(servletRequest,servletResponse);
-            }else if(path.equals("/homepage")){
-                filterChain.doFilter(servletRequest,servletResponse);
-            }else {
-                Object isLogin = ((HttpServletRequest) servletRequest).getSession().getAttribute("login");
-                if(isLogin!=null&&((String)isLogin).equals("login")){
-                    filterChain.doFilter(servletRequest,servletResponse);
-                }else{
-                    resp.sendRedirect("/login");
-                }
-            }
-        }
+//        HttpServletRequest requ = (HttpServletRequest)servletRequest;
+//        HttpServletResponse resp = (HttpServletResponse)servletResponse;
+//        String path = requ.getRequestURI();
+//        String method = requ.getMethod().toLowerCase();
+//        if(path.endsWith(".html")||path.endsWith(".css")||path.endsWith(".js")||path.endsWith(".png")||path.endsWith(".jpg")||path.endsWith(".db ")){
+//            filterChain.doFilter(servletRequest,servletResponse);
+//            return ;
+//        }
+//        if(method.equals("post")){
+//            Object username = requ.getSession().getAttribute("username");
+//            Object password = requ.getSession().getAttribute("password");
+//            if(username!=null&&password!=null){
+//                filterChain.doFilter(servletRequest,servletResponse);
+//            }else{
+//                resp.sendRedirect("/index");
+//            }
+//        }else{
+//            if(path.equals("/index")){
+//                filterChain.doFilter(servletRequest,servletResponse);
+//            }else if(path.equals("/homepage")){
+//                filterChain.doFilter(servletRequest,servletResponse);
+//            }else {
+//                Object isLogin = ((HttpServletRequest) servletRequest).getSession().getAttribute("index");
+//                if(isLogin!=null&&((String)isLogin).equals("index")){
+//                    filterChain.doFilter(servletRequest,servletResponse);
+//                }else{
+//                    resp.sendRedirect("/index");
+//                }
+//            }
+//        }
+        filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
