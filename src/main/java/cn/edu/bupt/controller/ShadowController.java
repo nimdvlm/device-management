a04b0fe06ab6f38b7c3d8ca54bb03f554def5237
cc19931002 <1181018082@qq.com>
2017-12-24 19:36:21 +0800

    device controller 返回device列表增加deviceId

--- src/main/java/cn/edu/bupt/controller/ShadowController.java
+++ src/main/java/cn/edu/bupt/controller/ShadowController.java
@@ -1,9 +1,17 @@
 package cn.edu.bupt.controller;
 
+import cn.edu.bupt.utils.HttpUtil;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/12/23.
@@ -17,6 +25,46 @@ public class ShadowController {
 
     @Value("${bupt.thingsboard.port}")
     String thingsboardPort ;
+
+    @Autowired
+    HttpServletRequest request;
+
+    @RequestMapping("/api/shadow/{deviceId}")
+    public String getDeviceShadow(@PathVariable("deviceId") String deviceId){
+        String url = getServer()+"/api/shadow/"+deviceId;
+        JsonObject body = new JsonObject();
+        body.addProperty("methodName","get");
+        JsonObject res = new JsonObject();
+        try{
+            String s = HttpUtil.sendPostToThingsboard(url,null,body,request.getSession());
+            res.addProperty("responce_code",0);
+            res.add("responce_msg",new JsonParser().parse(s));
+            return res.toString();
+        }catch(Exception e){
+            res.addProperty("responce_code",1);
+            res.addProperty("responce_msg","get deviceShadow failed");
+            return res.toString();
+        }
+    }
+
+    @RequestMapping("/api/shadow/control/{deviceId}")
+    public String controlDevice(@RequestBody String bd,@PathVariable("deviceId") String deviceId){
+        String url = getServer()+"/api/shadow/"+deviceId;
+        JsonObject body = new JsonObject();
+        body.addProperty("methodName","serviceCall");
+        body.add("serviceBody",new JsonParser().parse(bd));
+        JsonObject res = new JsonObject();
+        try{
+            String s = HttpUtil.sendPostToThingsboard(url,null,body,request.getSession());
+            res.addProperty("responce_code",0);
+            res.addProperty("responce_msg",s);
+            return res.toString();
+        }catch(Exception e){
+            res.addProperty("responce_code",1);
+            res.addProperty("responce_msg","control failed");
+            return res.toString();
+        }
+    }
 
     private String getServer() {
         return thingsboardHost+":"+thingsboardPort ;
