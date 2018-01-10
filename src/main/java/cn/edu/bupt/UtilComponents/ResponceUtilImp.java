61faeb9c8eb1c780cb0c6c7ce81ecf6356ce8bee
tangjialiang <phx108@sina.com>
2018-01-10 11:44:35 +0800

    tjl: Response类

--- src/main/java/cn/edu/bupt/UtilComponents/ResponceUtilImp.java
+++ src/main/java/cn/edu/bupt/UtilComponents/ResponceUtilImp.java
@@ -1,7 +1,29 @@
 package cn.edu.bupt.UtilComponents;
 
+import cn.edu.bupt.utils.ResponceUtil;
+import com.google.gson.JsonObject;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by tangjialiang on 2018/1/10.
+ *
  */
-public class ResponceUtilImp {
+
+@Component
+public class ResponceUtilImp implements ResponceUtil{
+    @Override
+    public String onSuccess(String msg) {
+        JsonObject errorInfoJson = new JsonObject() ;
+        errorInfoJson.addProperty("responce_code", 0);
+        errorInfoJson.addProperty("responce_msg", msg);
+        return errorInfoJson.toString() ;
+    }
+
+    @Override
+    public String onFail(String msg) {
+        JsonObject errorInfoJson = new JsonObject() ;
+        errorInfoJson.addProperty("responce_code", 1);
+        errorInfoJson.addProperty("responce_msg", msg);
+        return errorInfoJson.toString() ;
+    }
 }
