cf67f21ee0cfefce7dbfa5f17b6e2bf7f51e83e4
rainrun <33125415+rainrun@users.noreply.github.com>
2018-01-14 19:51:54 +0800

    whr: 新增加获取单个设备信息的接口

--- src/main/java/cn/edu/bupt/controller/ThreeDimensionController.java
+++ src/main/java/cn/edu/bupt/controller/ThreeDimensionController.java
@@ -9,29 +9,47 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/3Dwebgis")
 @Slf4j
-public class ThreeDimensionController extends DefaultThingsboardAwaredController{
+public class ThreeDimensionController extends DefaultThingsboardAwaredController {
 
     public static final String DEVICE_ID = "deviceId";
+
     @RequestMapping(value = "/all3Ddevices", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
     @ResponseBody
-    private String getthreeDdevices(){
+    private String getthreeDdevices() {
         String requestAddr = "/api/tenant/devices?limit=20";
 
         String responseContent = null;
-        try{
-            responseContent = HttpUtil.sendGetToThingsboard("http://"+getServer()+ requestAddr,
+        try {
+            responseContent = HttpUtil.sendGetToThingsboard("http://" + getServer() + requestAddr,
                     null,
                     request.getSession());
-        }catch(Exception e){
+        } catch (Exception e) {
             return retFail(e.toString());
         }
 
-        try{
-            JsonArray threeDdeviceJsonArr = (JsonArray)DeviceInfoDecode.deviceArr(responseContent);
+        try {
+            JsonArray threeDdeviceJsonArr = (JsonArray) DeviceInfoDecode.deviceArr(responseContent);
             return retSuccess(threeDdeviceJsonArr.toString());
-        }catch (Exception e){
+        } catch (Exception e) {
             return retFail(e.toString());
         }
     }
 
+    @RequestMapping(value = "/device/{deviceId}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String searchDevice(@PathVariable("deviceId") String dId){
+        String requestAddr = String.format("/api/device/%s", dId);
+
+        String responseContent = null ;
+        try {
+            responseContent = HttpUtil.sendGetToThingsboard("http://" + getServer() + requestAddr,
+                    null,
+                    request.getSession()) ;
+        } catch (Exception e) {
+            return retFail(e.toString()) ;
+        }
+        return retSuccess(responseContent) ;
+    }
 }
+
+
+
