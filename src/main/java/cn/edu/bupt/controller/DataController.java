83a9c765025f283033a94d2d112cc41e0cac59db
youzi <liyou@bupt.edu.cn>
2018-01-15 11:55:23 +0800

    liyou:获取历史数据的接口

---
+++ src/main/java/cn/edu/bupt/controller/DataController.java
@@ -0,0 +1,51 @@
+package cn.edu.bupt.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import cn.edu.bupt.utils.HttpUtil;
+
+/**
+ * Created by liyou on 2018/1/15.
+ */
+
+@RestController
+@RequestMapping("/api/data")
+public class DataController extends DefaultThingsboardAwaredController{
+
+    @RequestMapping(value="/getHistoricalData/{deviceId}/{startTime}/{endTime}", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
+    public String getHistoricalData(@PathVariable("deviceId") String deviceId, @PathVariable("startTime") String startTime, @PathVariable("endTime") String endTime)
+    {
+        String requestKeyAddr = "/api/plugins/telemetry/DEVICE/"+deviceId+"/keys/TIMESERIES";
+
+        String responseKeyContent = null ;
+        try {
+            responseKeyContent = HttpUtil.sendGetToThingsboard("http://" + getServer() + requestKeyAddr,
+                    null,
+                    request.getSession());
+        } catch (Exception e) {
+            return retFail(e.toString()) ;
+        }
+
+        responseKeyContent=responseKeyContent.replaceAll("[\\[\\]]","");
+        responseKeyContent=responseKeyContent.replaceAll("\"","");
+
+        String requestHistoricalDataAddr = "/api/plugins/telemetry/DEVICE/"+ deviceId
+                + "/values/TIMESERIES?keys=" +responseKeyContent
+                + "&startTs="+startTime
+                + "&endTs="+endTime
+                + "&interval=0&limit=100";
+
+        String responseHistoricalDataContent = null ;
+        try {
+            responseHistoricalDataContent = HttpUtil.sendGetToThingsboard("http://" + getServer() + requestHistoricalDataAddr,
+                    null,
+                    request.getSession());
+        } catch (Exception e) {
+            return retFail(e.toString()) ;
+        }
+
+        return retSuccess(responseHistoricalDataContent);
+    }
+}
