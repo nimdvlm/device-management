d09d6d0c20e4fcf7e6823bb9201f78f7d83c1dbe
miaomiao1024 <1024332886@qq.com>
2018-05-10 15:34:21 +0800

    前端与后台连接

--- src/main/resources/static/thingsManage/assets/js/serviceCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/serviceCtrl.js
@@ -1,54 +1,12 @@
-mainApp.controller("serviceCtrl",function ($scope,$resource) {
-
-
-});
-/*mainApp.filter('search',function () {
-        return function (input,serviceName) {
-            var result = [];
-            console.log("serviceName param=" + serviceName);
-            console.log("arguments[1] param="+ arguments[1]);
-            if(typeof (serviceName)!= 'undefined'&& serviceName!=''){
-                angular.forEach(input,function (item) {
-                    if(item.serviceName==serviceName){
-                        result.push(item);
-                    }
-                });
-                return result;
-            }
-            return input;
-        };
+mainApp.controller("abilityCtrl",function ($scope,$resource) {
+    /*锟斤拷取锟斤拷锟斤拷锟斤拷锟斤拷息锟斤拷锟斤拷展示
+    $scope.showAbility = true;
+    var abilityGroup = $resource('/api/v1/abilityGroup');
+    $scope.abilityGroups = abilityGroup.query(function(){
+        //锟斤拷始锟斤拷锟揭诧拷锟斤拷图
+        $scope.itemAM = $scope.abilityGroups[0];
     });*/
-/*过滤器var mainApp = angular.module("mainApp",[]);
-var mainApp = angular.module("mainApp",[]);
-mainApp.controller("serviceCtrl",function ($scope) {
-    $scope.managements = [{vendor:216,equipmentType:"设备1",equipmentModel:"Tpss"},
-        {vendor:215,equipmentType:"设备2",equipmentModel:"Tps"},
-        {vendor:214,equipmentType:"设备3",equipmentModel:"Tp"},
-        {vendor:213,equipmentType:"设备4",equipmentModel:"T"}]
-});
-app.filter('search',function () {
-    return function (input,equipmentType) {
-        var result = [];
-        console.log("equipmentType param=" + equipmentType);
-        console.log("arguments[1] param="+ arguments[1]);
-        if(typeof (equipmentType)!= 'undefined'&& equipmentType!=''){
-            angular.forEach(input,function (item) {
-                if(item.equipmentType==equipmentType){
-                    result.push(item);
-                }
-            });
-            return result;
-        }
-        return input;
-    };
-});
-//获取服务管理组展示
-    $scope.showModel = true;
-    var serviceManagement = $resource('/api/service/manufactures');
-    $scope.serviceManagements = serviceManagement.query(function(){
-        //显示模块
-        $scope.item = $scope.serviceManagements[0];
-    });*/
-/*删除服务组的js代码deleteSM()方法*/
 
 
+
+});
