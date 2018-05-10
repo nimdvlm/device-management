ba4c4b82e27a8d5c811a49293c49f51588f23b34
ly547165949 <l19960205>
2018-05-10 20:18:15 +0800

    设备token、设备更新、设备分配；登录页面修改密码

--- src/main/resources/static/thingsManage/assets/js/deviceListCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/deviceListCtrl.js
@@ -1,5 +1,5 @@
 
-mainApp.controller("deviceListCtrl",["$scope","$resource","$http",function ($scope,$resource,$http) {
+mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$resource) {
     var deviceInfo;//用于记录当前选中的设备
 
     /*设备列表信息获取与展示*/
@@ -20,26 +20,35 @@ mainApp.controller("deviceListCtrl",["$scope","$resource","$http",function ($sco
     };
 
 
-    /*新增设备*/
+    /*创建设备*/
     var createDeviceObj =  $resource("/api/device/create");
     $scope.createDevice = function(){
-        $scope.name = $("#name").val();
-        $scope.parent = $("#parentId").val();
-        $scope.deviceType = $("#deviceType").val();
-        $scope.manufacture = $("#manufacture").val();
-        $scope.model = $("#model").val();
-        $scope.location = $("#location").val();
-        $scope.status = $("#status").val();
-        $scope.createDeviceInfo = '{"name":'+'"'+$scope.name+'"'+',"parentDeviceId":'+'"'+$scope.parent+'"'+',"deviceType":'+'"'+$scope.deviceType+'"'+',"manufacture":'+'"'+$scope.manufacture+'"'+',"model":'+'"'+$scope.model+'"'+',"location":'+'"'+$scope.location+'"'+',"status":'+'"'+$scope.status+'"'+'}';
-        console.log($scope.createDeviceInfo);
-        $scope.deviceInformation = createDeviceObj.save({},$scope.createDeviceInfo,function (resp) {
-            toastr.success("新增设备成功！");
-            setTimeout(function () {
+        if($("#name").val()){
+            $scope.name = $("#name").val();
+            $scope.parent = $("#parentId").val();
+            $scope.deviceType = $("#deviceType").val();
+            $scope.manufacture = $("#manufacture").val();
+            $scope.model = $("#model").val();
+            $scope.location = $("#location").val();
+            $scope.status = $("#status").val();
+            $scope.createDeviceInfo = '{"name":'+'"'+$scope.name+'"'+',"parentDeviceId":'+'"'+$scope.parent+'"'+',"deviceType":'+'"'+$scope.deviceType+'"'+',"manufacture":'+'"'+$scope.manufacture+'"'+',"model":'+'"'+$scope.model+'"'+',"location":'+'"'+$scope.location+'"'+',"status":'+'"'+$scope.status+'"'+'}';
+            //字符串类型的数据发送给后台是会自动加上引号
+            console.log($scope.createDeviceInfo);
+            $scope.deviceInformation = createDeviceObj.save({},$scope.createDeviceInfo,function (resp) {
                 window.location.reload();
-            },500);
-        },function (error) {
-            toastr.error("新增设备失败！");
-        });
+            },function (error) {
+                toastr.error("新增设备失败！");
+            });
+        }
+        else{
+            /*增加提示效果*/
+            $("#name").addClass("input-err");
+            $("#modalConfirm").removeAttr("data-dismiss");
+            $('#name').on('focus', function() {
+                $(this).removeClass('input-err');
+            });
+        }
+
     };
 
     /*删除设备*/
@@ -48,10 +57,7 @@ mainApp.controller("deviceListCtrl",["$scope","$resource","$http",function ($sco
         console.log(deviceInfo);
         console.log(deviceInfo.id);
         $scope.deleteDevice = deleteDeviceObj.delete({deviceId:deviceInfo.id},{},function (resp) {
-            toastr.success("删除设备成功！");
-            setTimeout(function () {
                 window.location.reload();
-            },500);
         },function (error) {
             toastr.error("删除设备失败！");
         });
@@ -59,13 +65,13 @@ mainApp.controller("deviceListCtrl",["$scope","$resource","$http",function ($sco
 
     /*查看令牌*/
     var tokenObj = $resource("/api/device/token/:deviceId");
-    $scope.showToken = function () {
-        var tokenJson = tokenObj.get({deviceId:deviceInfo.id});
-        console.log(tokenJson);
-        $scope.token = tokenJson.credentialsId;
-        console.log(tokenJson.credentialsId);
-
-    }
+    $scope.showToken = function (){
+        $scope.tokenJson = tokenObj.get({deviceId:deviceInfo.id})
+            .$promise.then(function (value) {
+                $scope.token = value.deviceToken;
+                console.log($scope.token);
+            });
+    };
 
 
 
@@ -75,9 +81,53 @@ mainApp.controller("deviceListCtrl",["$scope","$resource","$http",function ($sco
 
 
     /*更新设备*/
-    // var refreshObj = $resource("");
+    var refreshDeviceObj = $resource("/api/device/updatedevice");
+    $scope.refreshDevice = function () {
+        $scope.reName = $("#reName").val();
+        $scope.reParent = $("#reParentId").val();
+        $scope.reDeviceType = $("#reDeviceType").val();
+        $scope.reManufacture = $("#reManufacture").val();
+        $scope.reModel = $("#reModel").val();
+        $scope.reLocation = $("#reLocation").val();
+        $scope.reStatus = $("#reStatus").val();
+        $scope.refreshDeviceInfo = '{"name":'+'"'+$scope.reName+'"'+',"Id":'+'"'+deviceInfo.id+'"'+',"parentDeviceId":'+'"'+$scope.reParentId+'"'+',"deviceType":'+'"'+$scope.reDeviceType+'"'+',"manufacture":'+'"'+$scope.reManufacture+'"'+',"model":'+'"'+$scope.reModel+'"'+',"location":'+'"'+$scope.reLocation+'"'+',"status":'+'"'+$scope.reStatus+'"'+'}';
+        //字符串类型的数据发送给后台是会自动加上引号
+        console.log($scope.refreshDeviceInfo);
+        $scope.refreshDeviceInformation = refreshDeviceObj.save({},$scope.refreshDeviceInfo,function (resp) {
+            toastr.success("更新设备成功！");
+            setTimeout(function () {
+                window.location.reload();
+            },1000);
+
+        },function (error) {
+            toastr.error("更新设备失败！");
+        });
+    };
+
+
+
+
 
     /*分配设备*/
+    var deviceGroupObj = $resource("/api/group/allgroups");
+    var deviceGroupAssignObj = $resource("/api/group/assign/:deviceId/:groupId");
+    $scope.deviceGroup = deviceGroupObj.query();
+    $scope.assignDeviceGroup = function(){
+        var groupId = $("#deviceGroupSelect option:selected").attr("id");
+        console.log("groupId:"+groupId);
+        console.log("deviceInfo:"+deviceInfo.id);
+        $scope.deviceGroupAssign = deviceGroupAssignObj.get({deviceId:deviceInfo.id,groupId:groupId},
+            function (resp) {
+            toastr.success("设备分配成功！");
+            },function (err) {
+            toastr.error("设备分配失败！");
+        });
+    };
+
+
+
+    /*厂商*/
+
 
 }]);
 
