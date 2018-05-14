2b056fa81dd623c351544a5abc9c5a8167a6ba17
ly547165949 <l19960205>
2018-05-14 17:03:21 +0800

    设备详情、父设备名称

--- src/main/resources/static/thingsManage/assets/js/deviceListCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/deviceListCtrl.js
@@ -1,13 +1,34 @@
 
 mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$resource) {
-    var deviceInfo;//用于记录当前选中的设备
+    $scope.deviceInfo;//用于记录当前选中的设备
+    var parentName;//用于记录父设备名称
+
 
     /*设备列表信息获取与展示*/
     var obj = $resource("/api/device/alldevices?limit=30");
-    $scope.deviceList = obj.query();//返回值为整个接口返回的内容
+    $scope.deviceList = obj.query();//返回值为限制个数的所有设备信息
     console.log($scope.deviceList);
+
+    /*在右侧表格中显示各个设备的信息*/
     $scope.show = function (data) {
-        deviceInfo = data;
+        //通过父设备ID获取父设备名称
+        $.ajax({
+            url:"/api/device/name/"+data.parentDeviceId,
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            type:"GET",
+            success:function(msg) {
+               parentName = msg;
+            }
+        });
+
+        /*
+       //angularjs会将string类型变量解析成object
+       var parentNameObj = $resource("/api/device/name/:parentId");
+        $scope.parentName = parentNameObj.get({parentId:data.parentDeviceId});
+        */
+
+        $scope.deviceInfo = data;
         console.log(data);
         $scope.ID = data.id;
         $scope.deviceName = data.name;
@@ -15,7 +36,8 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
         $scope.location = data.location;
         $scope.manufacture = data.manufacture;
         $scope.status = data.status;
-        $scope.parentId = data.parentDeviceId;
+        $scope.parentName = parentName;//父设备名
+        console.log("$scope.parentName:"+$scope.parentName);
         $scope.model = data.model;
     };
 
@@ -25,9 +47,9 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
     /*删除设备*/
     var deleteDeviceObj = $resource("/api/device/delete/:deviceId");
     $scope.delete = function(){
-        console.log(deviceInfo);
-        console.log(deviceInfo.id);
-        $scope.deleteDevice = deleteDeviceObj.delete({deviceId:deviceInfo.id},{},function (resp) {
+        console.log($scope.deviceInfo);
+        console.log($scope.deviceInfo.id);
+        $scope.deleteDevice = deleteDeviceObj.delete({deviceId:$scope.deviceInfo.id},{},function (resp) {
             toastr.success("删除设备成功！");
             setTimeout(function () {
                 window.location.reload();
@@ -41,7 +63,7 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
     /*查看令牌*/
     var tokenObj = $resource("/api/device/token/:deviceId");
     $scope.showToken = function (){
-        $scope.tokenJson = tokenObj.get({deviceId:deviceInfo.id})
+        $scope.tokenJson = tokenObj.get({deviceId:$scope.deviceInfo.id})
             .$promise.then(function (value) {
                 $scope.token = value.deviceToken;
                 console.log($scope.token);
@@ -57,14 +79,18 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
     $scope.assignDeviceGroup = function(){
         var groupId = $("#deviceGroupSelect option:selected").attr("id");
         console.log("groupId:"+groupId);
-        console.log("deviceInfo:"+deviceInfo.id);
-        $scope.deviceGroupAssign = deviceGroupAssignObj.get({deviceId:deviceInfo.id,groupId:groupId},
+        console.log("deviceInfo:"+$scope.deviceInfo.id);
+        $scope.deviceGroupAssign = deviceGroupAssignObj.get({deviceId:$scope.deviceInfo.id,groupId:groupId},
             function (resp) {
             toastr.success("设备分配成功！");
             },function (err) {
             toastr.error("设备分配失败！");
         });
     };
+
+
+
+
 
 
 
@@ -84,12 +110,54 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
      ============================================================ */
     /*获取厂商*/
 
-      $scope.getManufacture = function () {
+    /*设置初始信息*/
+    $scope.setValue = function (){
+        //设置父类设备初始信息
+        console.log("父类设备名称："+parentName);
+        $("#reParentId option").each(function () {
+            if($(this).val() == parentName){
+                $(this).attr("selected",true);
+            }
+        });
+
+
+        /*设置厂商初始信息*/
+        $("#reManufacture option").each(function () {
+            if($(this).val() == $scope.deviceInfo.manufacture){
+                $(this).attr("selected",true);
+            }
+        });
+
+
+        /*设置设备类型初始信息*/
+        $("#reDeviceType").prepend("<option class='select'>"+$scope.deviceInfo.deviceType+"</option>");
+        $("#reDeviceType .select").attr("selected",true);
+
+        /*设置型号初始信息*/
+        $("#reModel").prepend("<option class='select'>"+$scope.deviceInfo.model+"</option>");
+        $("#reModel .select").attr("selected",true);
+
+        /*设置位置初始信息*/
+        $("#reLocation").val($scope.deviceInfo.location);
+        /*设置状态初始信息*/
+        $("#reStatus").val($scope.deviceInfo.status);
+
+    };
+
+
+
+
+
+
+
+
+
+    $scope.getManufacture = function () {
         manufacturerId = $("#reManufacture option:selected").attr("class");
         console.log("厂商："+manufacturerId);
-
-
-        /*根据厂商查询设备类型*/
+        $("#reDeviceType option").remove();
+        $("#reModel option").remove();
+          /*根据厂商查询设备类型*/
         console.log("/api/v1/abilityGroup/deviceTypes?manufacturerId="+manufacturerId);
         var deviceTypeObj = $resource("/api/v1/abilityGroup/deviceTypes?manufacturerId="+manufacturerId);
         $scope.deviceTypeInfo = deviceTypeObj.query();
@@ -112,6 +180,7 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
         };
     };
 
+
     /*更新设备*/
     var refreshDeviceObj = $resource("/api/device/updatedevice");
     $scope.refreshDevice = function () {
@@ -122,7 +191,7 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
         $scope.reModel = $("#reModel").val();
         $scope.reLocation = $("#reLocation").val();
         $scope.reStatus = $("#reStatus").val();
-        $scope.refreshDeviceInfo = '{"name":'+'"'+$scope.reName+'"'+',"Id":'+'"'+deviceInfo.id+'"'+',"parentDeviceId":'+'"'+$scope.reParent+'"'+',"deviceType":'+'"'+$scope.reDeviceType+'"'+',"manufacture":'+'"'+$scope.reManufacture+'"'+',"model":'+'"'+$scope.reModel+'"'+',"location":'+'"'+$scope.reLocation+'"'+',"status":'+'"'+$scope.reStatus+'"'+'}';
+        $scope.refreshDeviceInfo = '{"name":'+'"'+$scope.reName+'"'+',"Id":'+'"'+$scope.deviceInfo.id+'"'+',"parentDeviceId":'+'"'+$scope.reParent+'"'+',"deviceType":'+'"'+$scope.reDeviceType+'"'+',"manufacture":'+'"'+$scope.reManufacture+'"'+',"model":'+'"'+$scope.reModel+'"'+',"location":'+'"'+$scope.reLocation+'"'+',"status":'+'"'+$scope.reStatus+'"'+'}';
         //字符串类型的数据发送给后台是会自动加上引号
         console.log($scope.refreshDeviceInfo);
         $scope.refreshDeviceInformation = refreshDeviceObj.save({},$scope.refreshDeviceInfo,function (resp) {
@@ -130,7 +199,6 @@ mainApp.controller("deviceListCtrl",["$scope","$resource",function ($scope,$reso
             setTimeout(function () {
                 window.location.reload();
             },1000);
-
         },function (error) {
             toastr.error("更新设备失败！");
         });
@@ -237,22 +305,37 @@ $scope.searchDevice = function () {
             })
         }
     });
+};
 
+/*显示详情*/
+$scope.showDetail = function () {
+    /*显示属性*/
+    var attrDetailObj = $resource("/api/data/getKeyAttribute/:deviceId");
+    $scope.attrDetailInfo = attrDetailObj.query({deviceId:$scope.deviceInfo.id});
+    console.log($scope.attrDetailInfo);
 
+    /*显示遥测数据*/
+    var realTimeObj = $resource("/api/data/getlatestData/:deviceId");
+    $scope.realTimeInfo = realTimeObj.query({deviceId:$scope.deviceInfo.id});
+    console.log($scope.realTimeInfo);
 };
 
 
+    /* =============================================================
+         jquery动画效果
+       ============================================================ */
 
 /* HIGHLIGHT效果*/
-$(document).ready(function () {
-    $(".highlight").mouseover(function () {
-        $(this).css("color","#337ab7");
-    });
-    $(".highlight").mouseout(function () {
-        $(this).css("color","#305680");
-    });
+    $(document).ready(function () {
+        $(".highlight").mouseover(function () {
+            $(this).css("color","#337ab7");
+        });
+        $(".highlight").mouseout(function () {
+            $(this).css("color","#305680");
+        });
 
-});
+
+    });
 
 
 
