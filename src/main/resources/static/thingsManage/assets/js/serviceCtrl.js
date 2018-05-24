e94dd7d5f07c14c3548b4846043607d06c823650
miaomiao1024 <1024332886@qq.com>
2018-05-24 20:50:04 +0800

    ability

--- src/main/resources/static/thingsManage/assets/js/serviceCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/serviceCtrl.js
@@ -9,10 +9,30 @@ mainApp.controller("abilityCtrl", function ($scope, $resource) {
     $scope.abilityGroups = abilityGroup.query();
     //console.log($scope.abilityGroups);
 
+    /*鼠标移入动画效果*/
+    $scope.fadeSiblings = function () {
+        $(".abilityBackgroundstyle").mouseover(function () {
+            $(this).siblings().stop().fadeTo(300, 0.3);
+        });
+    };
+    /*鼠标移出动画效果*/
+    $scope.reSiblings = function () {
+        $(".abilityBackgroundstyle").mouseout(function () {
+            $(this).siblings().stop().fadeTo(300, 1);
+        });
+    };
+
 
     /*右侧信息显示*/
     $scope.show = function(items){
         $scope.result = [];//初始化数组；
+        /*除点击元素外其他元素均无特殊样式*/
+        $scope.abilityGroups.forEach(function (item) {
+            if(items != item) item.style = {}
+        });
+        /*给点击元素加上特定样式*/
+        items.style = {"border": "2px solid #305680"};
+
         modelId = items.model.modelId;
         var abilitiesObj = $resource("/api/v1/ability/:modelId");
         $scope.abilitiesInfo = abilitiesObj.query({modelId:modelId})
