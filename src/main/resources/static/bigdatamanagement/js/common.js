3fb5468ef90f5152a07c40adb9a4a4eece202d47
yplokijuh <33217680+yplokijuh@users.noreply.github.com>
2018-09-04 08:33:43 +0800

    增加hdfs接口

--- src/main/resources/static/bigdatamanagement/js/common.js
+++ src/main/resources/static/bigdatamanagement/js/common.js
@@ -56,3 +56,13 @@ $('#ul4').click(
         d++;
     }
 );
+
+function formatDate(now) {
+    var year=now.getFullYear();
+    var month=now.getMonth()+1;
+    var date=now.getDate();
+    var hour=now.getHours();
+    var minute=now.getMinutes();
+    var second=now.getSeconds();
+    return year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
+}
