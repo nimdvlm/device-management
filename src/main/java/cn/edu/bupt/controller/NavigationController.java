cf193be56ce13bf0edf5cd8f0056d4aa1a8d4cb8
CZX <516713700@qq.com>
2018-09-21 09:46:52 +0800

    修改自动登录

--- src/main/java/cn/edu/bupt/controller/NavigationController.java
+++ src/main/java/cn/edu/bupt/controller/NavigationController.java
@@ -67,7 +67,7 @@ public class NavigationController {
     }
 
     @RequestMapping("/autoLogin")
-    public String  autoLogin(@RequestParam String username, @RequestParam String password,@RequestParam String module) {
+    public String  autoLogin(@RequestParam String username, @RequestParam String password,@RequestParam String module){
         HttpSession session = request.getSession();
         session.setAttribute("username", username);
         session.setAttribute("password", password);
@@ -75,9 +75,9 @@ public class NavigationController {
         String res = HttpUtil.getAccessToken(session);
         JsonObject responseJson = (JsonObject) new JsonParser().parse(res);
         if(responseJson.has("error")){
-            response.setStatus(400);
             session.removeAttribute("username");
             session.removeAttribute("password");
+            throw new IllegalArgumentException(responseJson.get("error_description").getAsString());
         }else if(responseJson.has("access_token")){
             UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(username,password);
             Subject subject = SecurityUtils.getSubject();
