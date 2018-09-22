799817afea1b6a8809cba406661e6d9a6cf755c8
梁思奇 <yellowpig1210@163.com>
2018-09-22 18:21:36 +0800

    LYH：组件库页-添加组件类型

--- src/main/resources/static/thingsManage/assets/js/widgetsLibraryCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/widgetsLibraryCtrl.js
@@ -186,10 +186,10 @@ mainApp.controller("widgetsLibraryCtrl", function ($scope, $resource) {
             title: {
                 text: '仪表盘',
                 left: "center",
-                top:'10px',
+                top: '10px',
                 textStyle: {
                     fontSize: 20,
-                    fontWeight:'normal',
+                    fontWeight: 'normal',
                     fontFamily: "Microsoft YaHei",
                     fontStyle: 'normal',
                     color: '#1964ad'
@@ -204,12 +204,12 @@ mainApp.controller("widgetsLibraryCtrl", function ($scope, $resource) {
                 radius: '70%',
                 center: ['50%', '60%'],
                 //detail: {formatter: '{value}%'},
-                detail:{show:false},
+                detail: {show: false},
                 data: [{value: 45, name: ''}],
-                axisLine:{lineStyle:{width:20}},
-                splitLine:{length:25},
+                axisLine: {lineStyle: {width: 20}},
+                splitLine: {length: 25},
                 pointer: {
-                    width:2
+                    width: 2
                 }
             }]
 
@@ -219,7 +219,273 @@ mainApp.controller("widgetsLibraryCtrl", function ($scope, $resource) {
             option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
             myChart.setOption(option, true);
         }, 4000);
-    }
 
+        //仪表盘1
+        var myChart1 = echarts.init(document.getElementById('Dashboard1'));
+        var option1 = {
+            title: {
+                text: '仪表盘一',
+                left: "center",
+                top: '10px',
+                textStyle: {
+                    fontSize: 20,
+                    fontWeight: 'normal',
+                    fontFamily: "Microsoft YaHei",
+                    fontStyle: 'normal',
+                    color: '#1964ad'
+                }
+            },
+            tooltip: { //弹窗组件
+                formatter: "{a} <br/>{b} : {c}%"
+            },
+            series: [{
+                name: '仪表盘1',
+                type: 'gauge',
+                radius: '70%',
+                center: ['50%', '60%'],
+                detail: {formatter: '{value}%', fontSize: '10'},
+                //detail:{show:false},
+                data: [{value: 45, name: ''}],
+                axisLine: {lineStyle: {width: 20, color: [[0.1, '#c23531'], [0.9, '#63869e'], [1, '#91c7ae']]}},
+                splitLine: {length: 25},
+                pointer: {
+                    width: 2
+                }
+            }]
+
+        };
+        myChart1.setOption(option1);
+        setInterval(function () {//把option.series[0].data[0].value的值使用random()方法获取一个随机数
+            option1.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+            myChart1.setOption(option1, true);
+        }, 4000);
+
+        //仪表盘2
+        var myChart2 = echarts.init(document.getElementById("Dashboard2"));
+        option2 = {
+            title: {
+                text: '仪表盘二',
+                left: "center",
+                top: '10px',
+                textStyle: {
+                    fontSize: 20,
+                    fontWeight: 'normal',
+                    fontFamily: "Microsoft YaHei",
+                    fontStyle: 'normal',
+                    color: '#1964ad'
+                }
+            },
+            //提示框组件。开发实际中去掉了指针，提示框可以不用设置。
+            tooltip: {
+                formatter: "{a} <br/>{b} : {c}%"
+            },
+            //下面属性才是仪表盘的核心！！反正我是这么认为的！！！
+            series: [{
+                //类型
+                type: 'gauge',
+                //半径
+                radius: 125,
+                //起始角度。圆心 正右手侧为0度，正上方为90度，正左手侧为180度。
+                startAngle: 180,
+                //结束角度。
+                endAngle: 0,
+                center: ['50%', '90%'],
+                //仪表盘轴线相关配置。
+                axisLine: {
+                    show: true,
+                    // 属性lineStyle控制线条样式
+                    lineStyle: {
+                        width: 40,
+                        color: [[1, '#63869e']]
+                    }
+                },
+                //分隔线样式。
+                splitLine: {
+                    show: false,
+                },
+                //刻度样式。
+                axisTick: {
+                    show: false,
+                },
+                //刻度标签。
+                axisLabel: {
+                    show: false,
+                },
+                //仪表盘指针。
+                pointer: {
+                    //这个show属性好像有问题，因为在这次开发中，需要去掉指正，我设置false的时候，还是显示指针，估计是BUG吧，我用的echarts-3.2.3；希望改进。最终，我把width属性设置为0，成功搞定！
+                    show: false,
+                    //指针长度
+                    length: '90%',
+                    width: 0,
+                },
+                //仪表盘详情，用于显示数据。
+                detail: {
+                    show: true,
+                    offsetCenter: [0, '-10%'],
+                    formatter: '{value}%',
+                    textStyle: {
+                        fontSize: 20
+                    }
+                },
+                data: [{
+                    value: 0,
+                    //   name: '存储量'
+                }]
+            }]
+        };
+        myChart2.setOption(option2);
+        //设置定时，加载随机数据，可以直观显示
+        timeTicket = setInterval(function () {
+            var random = (Math.random() * 100).toFixed(2);
+            var color = [[random / 100, '#63869e'], [1, '#c23531']];
+            option2.series[0].axisLine.lineStyle.color = color;
+            option2.series[0].data[0].value = random;
+            myChart2.setOption(option2, true);
+        }, 4000)
+        var myChart3 = echarts.init(document.getElementById("Dashboard3"));
+        option3 = {
+            title: {
+                text: '仪表盘三',
+                left: "center",
+                top: '10px',
+                textStyle: {
+                    fontSize: 20,
+                    fontWeight: 'normal',
+                    fontFamily: "Microsoft YaHei",
+                    fontStyle: 'normal',
+                    color: '#1964ad'
+                }
+            },
+            tooltip: { //弹窗组件
+                formatter: "{a} <br/>{b} : {c}%"
+            },
+            series: [
+                {
+                    type: "gauge",
+                    center: ["50%", "70%"], // 仪表位置
+                    radius: "100", //仪表大小
+                    startAngle: 200, //开始角度
+                    endAngle: -20, //结束角度
+                    axisLine: {
+                        show: false,
+                        lineStyle: { // 属性lineStyle控制线条样式
+                            color: [
+                                [0.5, new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                    offset: 1,
+                                    color: "#E75F25" // 50% 处的颜色
+                                }, {
+                                    offset: 0.8,
+                                    color: "#D9452C" // 40% 处的颜色
+                                }], false)], // 100% 处的颜色
+                                [0.7, new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                    offset: 1,
+                                    color: "#FFC539" // 70% 处的颜色
+                                }, {
+                                    offset: 0.8,
+                                    color: "#FE951E" // 66% 处的颜色
+                                }, {
+                                    offset: 0,
+                                    color: "#E75F25" // 50% 处的颜色
+                                }], false)],
+                                [0.9, new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 1,
+                                    color: "#C7DD6B" // 90% 处的颜色
+                                }, {
+                                    offset: 0.8,
+                                    color: "#FEEC49" // 86% 处的颜色
+                                }, {
+                                    offset: 0,
+                                    color: "#FFC539" // 70% 处的颜色
+                                }], false)],
+                                [1, new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                    offset: 0.2,
+                                    color: "#1CAD52" // 92% 处的颜色
+                                }, {
+                                    offset: 0,
+                                    color: "#C7DD6B" // 90% 处的颜色
+                                }], false)]
+                            ],
+                            width: 10
+                        }
+                    },
+                    splitLine: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLabel: {
+                        show: false
+                    },
+                    pointer: { //指针样式
+                        length: '45%'
+                    },
+                    detail: {
+                        show: false
+                    }
+                },
+                {
+                    type: "gauge",
+                    center: ["50%", "71%"], // 默认全局居中
+                    radius: "87.5",
+                    startAngle: 200,
+                    endAngle: -20,
+                    axisLine: {
+                        show: true,
+                        lineStyle: { // 属性lineStyle控制线条样式
+                            color: [ //表盘颜色
+                                [0.5, "#DA462C"],//0-50%处的颜色
+                                [0.7, "#FF9618"],//51%-70%处的颜色
+                                [0.9, "#FFED44"],//70%-90%处的颜色
+                                [1, "#20AE51"]//90%-100%处的颜色
+                            ],
+                            width: 18//表盘宽度
+                        }
+                    },
+                    splitLine: { //分割线样式（及10、20等长线样式）
+                        length: 30,
+                        lineStyle: { // 属性lineStyle控制线条样式
+                            width: 2
+                        }
+                    },
+                    axisTick: { //刻度线样式（及短线样式）
+                        length: 20
+                    },
+                    axisLabel: { //文字样式（及“10”、“20”等文字样式）
+                        color: "black",
+                        distance: 5 //文字离表盘的距离
+                    },
+                    detail: {
+                        formatter: '{value}%',
+                        offsetCenter: [0, "40%"],
+                        fontSize: 10,
+                        // backgroundColor: '#FFEC45',
+                        // height:30,
+                        /*  rich : {
+                         score : {
+                         //   color : "",
+                         fontFamily : "微软雅黑",
+                         fontSize : 10
+                         }
+                         }*/
+                    },
+                    data: [{
+                        value: 0,
+                        /*    label: {
+                         textStyle: {
+                         fontSize: 10
+                         }
+                         }*/
+                    }]
+                }
+            ]
+        };
+        myChart3.setOption(option3);
+        setInterval(function () {//把option.series[0].data[0].value的值使用random()方法获取一个随机数
+            option3.series[1].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+            myChart3.setOption(option3, true);
+        }, 4000);
+    }
 
 });
