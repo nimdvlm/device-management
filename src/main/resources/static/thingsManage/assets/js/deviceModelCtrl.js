56798de795da6248ffc01fe817f3672d524175dd
miaomiao1024 <1024332886@qq.com>
2018-09-27 21:13:03 +0800

    devict type默认图标显示

--- src/main/resources/static/thingsManage/assets/js/deviceModelCtrl.js
+++ src/main/resources/static/thingsManage/assets/js/deviceModelCtrl.js
@@ -5,22 +5,27 @@ mainApp.controller("deviceModelCtrl", function ($scope, $resource) {
     console.log("展示deviceType");
     console.log($scope.deviceTypeGroups);
 
-    /*鼠标移入动画效果*/
+    /*鼠标移入动画效果
     $scope.fadeSiblings = function () {
         $(".deviceTypeIconStyle").mouseover(function () {
             $(this).siblings().stop().fadeTo(300, 0.3);
         });
-    };
-    /*鼠标移出动画效果*/
+    };*/
+    /*鼠标移出动画效果
     $scope.reSiblings = function () {
         $(".deviceTypeIconStyle").mouseout(function () {
             $(this).siblings().stop().fadeTo(300, 1);
         });
+    };*/
+    $scope.showDefultIcon = false;
+    $scope.showIconPictrue = function () {
+        $scope.showDefultIcon = !$scope.showDefultIcon;
+        console.log( $scope.showDefultIcon);
     };
-
 
     /*删除*/
     $scope.deleteDeviceTypeIcon = function (item) {
+        console.log("点击删除出现字段");
         console.log(item);
         console.log(item.model.modelId);
         $scope.modelId = item.model.modelId;
@@ -38,15 +43,58 @@ mainApp.controller("deviceModelCtrl", function ($scope, $resource) {
         });
     };
 
+
+
+
+    var createDeviceModel = {};
+    createDeviceModel.icon = "";
+    $scope.imgSrcDeviceIcon1 = function () {
+        console.log("imgSrcDeviceIcon1");
+        createDeviceModel.icon = "../images/deviceIcon1.png";
+        $scope.deviceIcon1 = true;
+        $scope.deviceIcon2 = false;
+        $scope.deviceIcon3 = false;
+        $scope.title1 = false;
+        console.log(createDeviceModel.icon);
+        console.log($scope.deviceIcon1);
+    };
+    $scope.imgSrcDeviceIcon2 = function () {
+        console.log("imgSrcDeviceIcon2");
+        createDeviceModel.icon = "../images/deviceIcon2.jpg";
+        $scope.deviceIcon1 = false;
+        $scope.deviceIcon2 = true;
+        $scope.deviceIcon3 = false;
+        $scope.title1 = false;
+        console.log(createDeviceModel.icon);
+        console.log($scope.deviceIcon2);
+    };
+    $scope.imgSrcDeviceIcon3 = function () {
+        console.log("imgSrcDeviceIcon3");
+        createDeviceModel.icon = "../images/deviceIcon3.png";
+        $scope.deviceIcon1 = false;
+        $scope.deviceIcon2 = false;
+        $scope.deviceIcon3 = true;
+        $scope.title1 = false;
+        console.log(createDeviceModel.icon);
+        console.log($scope.deviceIcon3);
+    };
+    $scope.imgSrcTitle = function () {
+        console.log("imgSrcDeviceIcon4");
+        createDeviceModel.icon = "../images/title.icon";
+        $scope.deviceIcon1 = false;
+        $scope.deviceIcon2 = false;
+        $scope.deviceIcon3 = false;
+        $scope.title1 = true;
+        console.log(createDeviceModel.icon);
+        console.log($scope.title1);
+    };
     $scope.addDeviceModel = function () {
-        var createDeviceModel = {};
         createDeviceModel.manufacturerName = $("#manufacturerName").val();
         createDeviceModel.deviceType = $("#deviceType").val();
         createDeviceModel.model = $("#deviceModel").val();
-        createDeviceModel.icon = $("#potatoIcon").val();
+
         createDeviceModel.limit_lifetime = $("#limitTime").val();
-        console.log(createDeviceModel.limit_lifetime);
-        console.log("time");
+
         if (createDeviceModel.limit_lifetime == ""){
             createDeviceModel.limit_lifetime = 0;
         }else {
@@ -63,20 +111,25 @@ mainApp.controller("deviceModelCtrl", function ($scope, $resource) {
             var sixthNum = createDeviceModel.limit_lifetime.substring(7,8);
             console.log(sixthNum);
             const longNum = parseInt(firstNum)*10*3600000 + parseInt(secondNum)*3600000 + parseInt(thirdNum)*10*60000 + parseInt(fourthNum)*60000 + parseInt(fifthNum)*10*1000 + parseInt(sixthNum)*1000;
-
             console.log(longNum);
             createDeviceModel.limit_lifetime = longNum;
         }
-
+        if(createDeviceModel.icon == ""){
+            createDeviceModel.icon = "../images/deviceDefult.PNG";
+        }
+        console.log("createDeviceModel.icon");
+        console.log(createDeviceModel.icon);
         $scope.createDeviceModel = JSON.stringify(createDeviceModel);
+
+        console.log("查看存入是数值：");
         console.log($scope.createDeviceModel);
         var createDeviceGroupObj =  $resource("/api/devicetype/insert");
         $scope.deviceInfomation = createDeviceGroupObj.save({},$scope.createDeviceModel,function (resp) {
             console.log(resp);
-            /*toastr.success("创建成功！");
-            setTimeout(function () {
+            toastr.success("创建成功！");
+           setTimeout(function () {
                 window.location.reload();
-            },500);*/
+            },500);
         },function (error) {
             toastr.error("创建失败！");
         });
